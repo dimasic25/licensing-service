c1bc1aadf6d61a3b9a751206b008f0046f066364
Dmitry <dimonbubyr@mail.ru>
2022-07-07 10:58:42 +0300

    Initial commit

---
+++ src/main/java/com/demon/licensingservice/model/License.java
@@ -0,0 +1,17 @@
+package com.demon.licensingservice.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+public class License {
+
+    private int id;
+    private String licenseId;
+    private String description;
+    private String organizationId;
+    private String productName;
+    private String licenseType;
+
+}
---
+++ src/test/java/com/demon/licensingservice/LicensingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demon.licensingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
